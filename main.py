c08cf79696bce863113e0df8edf4585cdc4f3eb4
Julian <julian.limburg@gmx.de>
2022-05-12 18:50:49 +0200

    Testing HTML JS Python comms

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import psutil
 
+
 class Plugin:
     async def test(self, *args):
         battery = psutil.sensors_battery()
         return battery.percent
 
     # A normal method. It can be called from JavaScript using call_plugin_function("method_2", argument1, argument2)
-    async def method_2(self, *args):
-        pass
+    async def test2(self, *args):
+        return "A string from python"
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
