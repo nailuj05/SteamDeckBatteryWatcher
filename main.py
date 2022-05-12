feed6abb1ea2ac384a80da06ed847995ed0403e6
Julian <julian.limburg@gmx.de>
2022-05-12 19:06:35 +0200

    Updated to not use psutil

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-import psutil
+import os
 
 
 class Plugin:
-    async def test(self) -> str:
-        battery = psutil.sensors_battery()
-        return str(battery.percent)
+    async def GetCharge(self) -> int:
+        return int(read_from_sys("/sys/class/hwmon/hwmon2/device/charge_now", amount=-1).strip())
 
     # A normal method. It can be called from JavaScript using call_plugin_function("method_2", argument1, argument2)
-    async def test2(self) -> str:
+    async def PythonTest(self) -> str:
         return "A string from python"
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
         pass
+
+
+def read_from_sys(path, amount=1):
+    with open(path, mode="r") as f:
+        return f.read(amount)
