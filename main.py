502c8b28bbd10bbabb92dc0ba4f59834b3619b96
Julian <julian.limburg@gmx.de>
2022-05-12 22:51:08 +0200

    Try 2 approaches

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ class Plugin:
     async def GetCharge(self) -> str:
         b = psutil.sensors_battery()
         return str(b.percent)
-    # A normal method. It can be called from JavaScript using call_plugin_function("method_2", argument1, argument2)
 
-    async def GetCharge2(self) -> float:
-        b = psutil.sensors_battery()
-        return float(b.percent)
+    async def GetCharge2(self) -> int:
+        return int(read_from_sys("/sys/class/hwmon/hwmon2/device/charge_now", amount=-1).strip())
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
     async def _main(self):
