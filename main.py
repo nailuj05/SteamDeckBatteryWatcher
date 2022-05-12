6d186db93a82847fc7566c6dae2c1265ebb582f8
Julian <julian.limburg@gmx.de>
2022-05-12 18:53:51 +0200

    remove arguments

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import psutil
 
 
 class Plugin:
-    async def test(self, *args):
+    async def test(self):
         battery = psutil.sensors_battery()
         return battery.percent
 
     # A normal method. It can be called from JavaScript using call_plugin_function("method_2", argument1, argument2)
-    async def test2(self, *args):
+    async def test2(self):
         return "A string from python"
 
     # Asyncio-compatible long-running code, executed in a task when the plugin is loaded
